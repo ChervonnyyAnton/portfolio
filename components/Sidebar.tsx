0850175f42158ad6a5fea4245741605d8ad76da9
ChervonnyyAnton <chervonnyy@icloud.com>
2023-02-03 18:45:06 +0100

    small changes

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -30,10 +30,11 @@ const Sidebar: React.FC = () => {
       </p>
       <a
         className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full dark:bg-dark-200 font-kaushan"
-        href=""
-        download={""}
+        href="assets/chervonnyy_cv.pdf"
+        download={"chervonnyy_cv.pdf"}
       >
-        <GiTie className="w-6 h-6" />
+        <GiTie className="w-6 h-6" 
+        />
         Download Resume
       </a>
       <div className="flex justify-around w-9/12 mx-auto my-5 text-green md:w-full">
