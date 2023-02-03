331d4d0562c110d70f4a7010b16822361d3c692f
Chervonnyy Anton <94194066+ChervonnyyAnton@users.noreply.github.com>
2023-02-03 18:51:29 +0000

    hotfix links-icons social

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -38,10 +38,10 @@ const Sidebar: React.FC = () => {
         Download Resume
       </a>
       <div className="flex justify-around w-9/12 mx-auto my-5 text-green md:w-full">
-        <a href="https://www.linkedin.com/in/chervonnyy/" target="_blank" rel="noreferrer">
+        <a href=" https://github.com/ChervonnyyAnton" target="_blank" rel="noreferrer">
           <AiFillGithub className="w-8 h-8 cursor-pointer" />
         </a>
-        <a href="https://github.com/ChervonnyyAnton" target="_blank" rel="noreferrer">
+        <a href="https://www.linkedin.com/in/chervonnyy/" target="_blank" rel="noreferrer">
           <AiFillLinkedin className="w-8 h-8 cursor-pointer" />
         </a>
         <a href="https://www.youtube.com/@Pro100proQA" target="_blank" rel="noreferrer">
